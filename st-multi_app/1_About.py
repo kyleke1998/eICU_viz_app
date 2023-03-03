6d8877e3ec80fbff95ec965dcb4d8bcd4593f3f4
Kyle (Si yang) Ke <siyangke98@gmail.com>
2023-03-03 10:32:00 -0500

    Completed the first draft of the performance monitoring page.

--- st-multi_app/1_About.py
+++ st-multi_app/1_About.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
-
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 st.set_page_config(page_title="About",
@@ -9,6 +10,6 @@ layout="wide",
 initial_sidebar_state="expanded")
 
 
-logo = Image.open('./images/logo_2.png')
+logo = Image.open('../images/logo_2.png')
 st.image(logo)
 st.title('About')
